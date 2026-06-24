673dfd231939332372fec474b9e1d1fd8755d027
TayfunDanabas <danabas285@gmail.com>
2026-06-24 17:04:08 +0200

    images added

--- scripts/dishes.js
+++ scripts/dishes.js
@@ -1,25 +1,25 @@
 const dishes = [
   {
     "name": "Veggie mushroom black burger",
-    "image": "./assets/img/Dinner1.jpg",
+    "image": "./assets/img/Dinner 1.jpg",
     "price": 16.90,
     "ingredients": ["Mixed green salad", "Tomatoes", "Edamame", "Mushrooms"]
   },
   {
     "name": "All meat burger",
-    "image": "./assets/img/Dinner2.jpg",
+    "image": "./assets/img/Dinner 2.jpg",
     "price": 15.90,
     "ingredients": ["Beef", "Bacon", "Dill pickles", "Smoked cheese", "Ketchup", "BBQ sauce"]
   },
   {
     "name": "Beef red burger",
-    "image": "./assets/img/Dinner3.jpg",
+    "image": "./assets/img/Dinner 3.jpg",
     "price": 14.90,
     "ingredients": ["Beef", "Cheese", "Tomatoes", "Lettuce", "Onion"]
   },
   {
     "name": "Big chicken burger",
-    "image": "./assets/img/Dinner4.jpg",
+    "image": "./assets/img/Dinner 4.jpg",
     "price": 15.90,
     "ingredients": ["Chicken", "Cheese", "Tomatoes", "Lettuce", "Onion", "Bell pepper"]
   },
